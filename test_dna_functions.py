fa642b652b889b44a70cd48b236c1b2f0189f8dc
Dana Brosh <danabrosh4@gmail.com>
2025-07-03 14:38:13 +0300

    test file changed

--- test_dna_functions.py
+++ test_dna_functions.py
@@ -1,24 +1,22 @@
-# Tests the dna_to_mrna function with different DNA sequences.
+# Tests the dna_to_mrna function with DNA template strand input
 from dna_functions import dna_to_mrna
 
 def test_dna_to_mrna():
-    assert dna_to_mrna("ATG") == "AUG"
-    assert dna_to_mrna("TAC") == "UAC"
-    assert dna_to_mrna("CGT") == "CGU"
-    assert dna_to_mrna("XXX") == "XXX"  # stays the same if unknown
+    assert dna_to_mrna("ATG") == "CAU"
+    assert dna_to_mrna("TAC") == "GUA"
+    assert dna_to_mrna("CGT") == "ACG"
+    assert dna_to_mrna("XXX") == "NNN"  # 'X' not recognized, becomes 'N'
 
 
-
-#Tests the codon_translation function with different 3-letter DNA codons.
+# Tests the codon_translation function using DNA template strand
 from dna_functions import codon_translation
 
 def test_codon_translation():
-    assert codon_translation("ATG") == ("AUG", "Methionine")
-    assert codon_translation("TTC") == ("UUC", "Phenylalanine")
-    assert codon_translation("AAA") == ("AAA", "Lysine")
-    assert codon_translation("CCC") == ("CCC", "Proline")
-    assert codon_translation("GAA") == ("GAA", "Glutamic acid")
-
+    assert codon_translation("CAT") == ("AUG", "Methionine")
+    assert codon_translation("GAA") == ("UUC", "Phenylalanine")
+    assert codon_translation("TTT") == ("AAA", "Lysine")
+    assert codon_translation("GGG") == ("CCC", "Proline")
+    assert codon_translation("CTT") == ("AAG", "Lysine")
 
 # Test the find_motif_positions function
 from dna_functions import find_motif_positions
@@ -30,7 +28,7 @@ def test_find_motif_positions():
     assert find_motif_positions(seq, "AAA") == []  
 
 
-#Test the introduce_mutations function
+# Test the introduce_mutations function
 from dna_functions import introduce_mutations
 
 def test_introduce_mutations():
@@ -38,15 +36,14 @@ def test_introduce_mutations():
     num_mutations = 2
     mutated_seq, mutations = introduce_mutations(dna_seq, num_mutations)
 
-    # Make sure the mutated sequence is the same length as the original
+    # Ensure the mutated sequence is the same length as the original
     assert len(mutated_seq) == len(dna_seq)
 
-    # Check that we got the right number of mutations
+    # Check the number of mutations
     assert len(mutations) == num_mutations
 
-    # Make sure each mutation actually changed a base
+    # Ensure each mutation changed a base
     for pos, orig, new in mutations:
         assert dna_seq[pos] == orig
         assert mutated_seq[pos] == new
         assert orig != new
-
